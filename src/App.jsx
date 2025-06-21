28b5fa681a124f620c69662c3d0cdb9109fddf1e
Taras <luchakt2@gmail.com>
2025-06-21 13:15:45 +0300

    Avoid mutating state by copying goodsFromServer into state

--- src/App.jsx
+++ src/App.jsx
@@ -17,77 +17,70 @@ export const goodsFromServer = [
 ];
 
 export const App = () => {
-
-  const [visibleGoods, setVisibleGoods] = useState(goodsFromServer);
+  const [visibleGoods, setVisibleGoods] = useState([...goodsFromServer]);
   const sortByAlphabetically = () => {
     setVisibleGoods(
-      ([...visibleGoods]).sort((good1, good2) => good1.localeCompare(good2))
-    )
-  }
+      [...visibleGoods].sort((good1, good2) => good1.localeCompare(good2)),
+    );
+  };
 
   const sortByLength = () => {
     setVisibleGoods(
-      ([...visibleGoods]).sort((good1, good2) => good1.length - good2.length)
-    )
-  }
+      [...visibleGoods].sort((good1, good2) => good1.length - good2.length),
+    );
+  };
 
   const reverse = () => {
-    setVisibleGoods(
-      visibleGoods.toReversed()
-    )
-  }
+    setVisibleGoods(visibleGoods.toReversed());
+  };
 
   const reset = () => {
     setVisibleGoods(goodsFromServer);
-  }
+  };
 
   return (
-  <div className="section content">
-
-    <div className="buttons">
-      <button
-        type="button"
-        className="button is-info is-light"
-        onClick={sortByAlphabetically}
-      >
-        Sort alphabetically
-      </button>
-
-      <button
-        type="button"
-        className="button is-success is-light"
-        onClick={sortByLength}
-      >
-        Sort by length
-      </button>
-
-      <button
-        type="button"
-        className="button is-warning is-light"
-        onClick={reverse}
-      >
-        Reverse
-      </button>
-
-      <button
-        type="button"
-        className="button is-danger is-light"
-        onClick={reset}
-      >
-        Reset
-      </button>
-    </div>
-
-    <ul>
-      {visibleGoods.map(good => (
-        <li
-          key={good}
-          data-cy="Good"
+    <div className="section content">
+      <div className="buttons">
+        <button
+          type="button"
+          className="button is-info is-light"
+          onClick={sortByAlphabetically}
         >
-          {good}
-        </li>
-      ))}
-    </ul>
-  </div>
-  )
-}
+          Sort alphabetically
+        </button>
+
+        <button
+          type="button"
+          className="button is-success is-light"
+          onClick={sortByLength}
+        >
+          Sort by length
+        </button>
+
+        <button
+          type="button"
+          className="button is-warning is-light"
+          onClick={reverse}
+        >
+          Reverse
+        </button>
+
+        <button
+          type="button"
+          className="button is-danger is-light"
+          onClick={reset}
+        >
+          Reset
+        </button>
+      </div>
+
+      <ul>
+        {visibleGoods.map(good => (
+          <li key={good} data-cy="Good">
+            {good}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
